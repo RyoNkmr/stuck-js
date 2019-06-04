f20ad7b960b2b460603d27067e6910edc3f0c374
RyoNkmr <ryonakamuraryo@gmail.com>
2019-06-04 21:13:23 +0900

    style(eslint): setup eslint for TypeScript with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,38 @@
 module.exports = {
+  env: {
+    browser: true,
+  },
   extends: [
     'eslint:recommended',
     'airbnb-base',
+    'standard',
+    'prettier',
+    'prettier/standard',
+    'prettier/@typescript-eslint',
   ],
-  parser: 'babel-eslint',
-  plugins: [
-    'import',
-    'flowtype',
-  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: false,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['import', '@typescript-eslint'],
   rules: {
+    'lines-between-class-members': 'off',
+    /*
+     * https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-478389548
+     */
+    '@typescript-eslint/no-for-in-array': 'off',
+    '@typescript-eslint/no-unnecessary-qualifier': 'off',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'off',
+    '@typescript-eslint/promise-function-async': 'off',
+    '@typescript-eslint/restrict-plus-operands': 'off',
+    // ======================================================
     'arrow-parens': ['error', 'as-needed'],
-    'flowtype/define-flow-type': 1,
-    'flowtype/use-flow-type': 1,
     'no-param-reassign': ['error', { props: false }],
-    'operator-linebreak': [
-      'error',
-      'before',
-      {
-        overrides: {
-          '=': 'after',
-        },
-      },
-    ],
   },
   globals: {
     window: true,
