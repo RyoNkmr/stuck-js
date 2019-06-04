ab4f56e9b6827f7115c941193adfeb92236ba35c
RyoNkmr <ryonakamuraryo@gmail.com>
2019-06-04 22:49:04 +0900

    test(specs): wait for changing viewport in test

--- specs/puppeteerHelper.js
+++ specs/puppeteerHelper.js
@@ -1,22 +1,38 @@
 export const scrollTo = async (scrollLeft = 0, scrollTop = 0) => {
-  await page.evaluate((left, top) => { window.scroll(left, top); }, scrollLeft, scrollTop);
+  await page.evaluate(
+    (left, top) => {
+      window.scroll(left, top);
+    },
+    scrollLeft,
+    scrollTop
+  );
   await page.waitFor(60);
 };
 
-export const getRects = (...selectors) => (
-  page.evaluate(targetSelectors => (
-    targetSelectors
-      .reduce((acc, selector) => acc.concat(Array.from(document.querySelectorAll(selector))), [])
-      .map(el => el.getBoundingClientRect())
-      .map(({ top, left, width, height }) => ({ top, left, width, height }))
-  ), selectors)
-);
+export const getRects = (...selectors) =>
+  page.evaluate(
+    targetSelectors =>
+      targetSelectors
+        .reduce(
+          (acc, selector) =>
+            acc.concat(Array.from(document.querySelectorAll(selector))),
+          []
+        )
+        .map(el => el.getBoundingClientRect())
+        .map(({ top, left, width, height }) => ({ top, left, width, height })),
+    selectors
+  );
 
-export const getParentRects = (...selectors) => (
-  page.evaluate(targetSelectors => (
-    targetSelectors
-      .reduce((acc, selector) => acc.concat(Array.from(document.querySelectorAll(selector))), [])
-      .map(el => el.parentNode.getBoundingClientRect())
-      .map(({ top, left, width, height }) => ({ top, left, width, height }))
-  ), selectors)
-);
+export const getParentRects = (...selectors) =>
+  page.evaluate(
+    targetSelectors =>
+      targetSelectors
+        .reduce(
+          (acc, selector) =>
+            acc.concat(Array.from(document.querySelectorAll(selector))),
+          []
+        )
+        .map(el => el.parentNode.getBoundingClientRect())
+        .map(({ top, left, width, height }) => ({ top, left, width, height })),
+    selectors
+  );
